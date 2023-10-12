7d44f18b4d49729f4adf163d3cea348fe798127a
mint3382 <mskim0926@naver.com>
2023-10-12 21:36:55 +0900

    feat: ChatViewController 추가

---
+++ PetLanguage/PetLanguage/ChatViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ChatViewController.swift
+//  PetLanguage
+//
+//  Created by minsong kim on 10/12/23.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
