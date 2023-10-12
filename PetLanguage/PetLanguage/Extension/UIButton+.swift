0047720bd4107d0de022f09cd76462c5574ecc32
mint3382 <mskim0926@naver.com>
2023-10-13 03:29:42 +0900

    feat: UIButton extension에 파라미터가 다른 convenience init 추가

--- PetLanguage/PetLanguage/Extension/UIButton+.swift
+++ PetLanguage/PetLanguage/Extension/UIButton+.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 extension UIButton {
-    convenience init(title: String, color: UIColor, action: (() -> UIAction)) {
+    convenience init(title: String?, color: UIColor, action: (() -> UIAction)) {
         self.init(frame: .zero)
         self.init(primaryAction: action())
         self.setTitle(title, for: .normal)
         self.setTitleColor(.white, for: .normal)
         self.backgroundColor = color
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    convenience init(action: (() -> UIAction)) {
+        self.init(frame: .zero)
+        self.init(primaryAction: action())
+        self.setImage(UIImage(systemName: "paperplane"), for: .normal)
         self.translatesAutoresizingMaskIntoConstraints = false
     }
 }
