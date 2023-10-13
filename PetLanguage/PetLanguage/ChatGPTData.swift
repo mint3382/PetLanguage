8c25df4c49b6d65ba8f27996f733b34ca1dd4b97
mint3382 <mskim0926@naver.com>
2023-10-13 23:38:45 +0900

    feat: ChatGPTData Model 추가

---
+++ PetLanguage/PetLanguage/ChatGPTData.swift
@@ -0,0 +1,45 @@
+//
+//  ChatGPTData.swift
+//  PetLanguage
+//
+//  Created by minsong kim on 10/13/23.
+//
+
+import Foundation
+
+struct ChatGPTData: Decodable {
+    let choices: [Choice]
+    let created: Int
+    let id: String
+    let model: String
+    let object: String
+    let usage: Usage
+}
+
+struct Choice: Decodable {
+    let finishReason: String
+    let index: Int
+    let message: Message
+
+    enum CodingKeys: String, CodingKey {
+        case finishReason = "finish_reason"
+        case index, message
+    }
+}
+
+struct Message: Decodable {
+    let content:String
+    let role: String
+}
+
+struct Usage: Decodable {
+    let completionTokens: Int
+    let promptTokens: Int
+    let totalTokens: Int
+
+    enum CodingKeys: String, CodingKey {
+        case completionTokens = "completion_tokens"
+        case promptTokens = "prompt_tokens"
+        case totalTokens = "total_tokens"
+    }
+}
