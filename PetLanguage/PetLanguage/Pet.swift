91e1d9ea90cda8544ac75530abaf8bfbaa55c6bf
mint3382 <mskim0926@naver.com>
2023-10-13 11:23:53 +0900

    feat: Pet 구조체 추가

---
+++ PetLanguage/PetLanguage/Pet.swift
@@ -0,0 +1,14 @@
+//
+//  Pet.swift
+//  PetLanguage
+//
+//  Created by minsong kim on 10/13/23.
+//
+
+import Foundation
+
+struct Pet {
+    let name: String
+    let age: Int
+    let species: String
+}
