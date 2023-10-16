418b47b823a6848a3b4e6898fedba3c04b35d9c1
mint3382 <mskim0926@naver.com>
2023-10-16 22:18:30 +0900

    refactor: Label에 Padding을 주는 customLabel로 변경

---
+++ PetLanguage/PetLanguage/View/PaddingLabel.swift
@@ -0,0 +1,35 @@
+//
+//  PaddingLabel.swift
+//  PetLanguage
+//
+//  Created by minsong kim on 10/15/23.
+//
+
+import UIKit
+
+final class PaddingLabel: UILabel {
+    private var padding = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    
+    convenience init(backgroundColor: UIColor) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 8
+        self.font = .preferredFont(forTextStyle: .caption2)
+        self.numberOfLines = 0
+        self.preferredMaxLayoutWidth = 300
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: padding))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += padding.top + padding.bottom
+        contentSize.width += padding.left + padding.right
+
+        return contentSize
+    }
+}
