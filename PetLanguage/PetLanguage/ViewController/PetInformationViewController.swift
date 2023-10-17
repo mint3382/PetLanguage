e7864d040504f4d1e68e3574021e4d836f668535
mint3382 <mskim0926@naver.com>
2023-10-17 13:41:42 +0900

    refactor: age 입력받는 방법 pickerView로 변경, species 입력 방법 segmented control로 변경

--- PetLanguage/PetLanguage/ViewController/PetInformationViewController.swift
+++ PetLanguage/PetLanguage/ViewController/PetInformationViewController.swift
@@ -9,28 +9,32 @@ import UIKit
 
 class PetInformationViewController: UIViewController {
     var pet: Pet?
+    var age: Int = 0
     
-    let logoImage = UIImageView(file: "logo")
+    let mainImage = UIImageView(file: "logo")
     let nameImage = UIImageView(file: "name")
     let ageImage = UIImageView(file: "age")
     let speciesImage = UIImageView(file: "species")
     
     let nameTextField = UITextField(placeholder: "이름")
-    let ageTextField = UITextField(placeholder: "나이")
-    
-    var speciesButton = UIButton(title: "동물 선택", color: .systemBrown)
     var startButton = UIButton()
     
+    lazy var speciesSegmentControl = UISegmentedControl(items: [catAction, dogAction])
     lazy var catAction = UIAction(title: "냥냥?", handler: { action in
-        self.settingPet(type: .cat)
+        self.settingMainImage(type: .cat)
     })
     lazy var dogAction = UIAction(title: "멍멍?", handler: { action in
-        self.settingPet(type: .dog)
+        self.settingMainImage(type: .dog)
     })
     lazy var pushNextAction = UIAction() { action in
-        guard let pet = self.pet else {
-            return
+        var pet: Pet
+        
+        if self.speciesSegmentControl.selectedSegmentIndex == 0 {
+            pet = self.settingPet(type: .cat)
+        } else {
+            pet = self.settingPet(type: .dog)
         }
+        
         let nextViewController = ChatViewController(pet: pet)
         
         self.navigationController?.pushViewController(nextViewController, animated: true)
@@ -40,34 +44,38 @@ class PetInformationViewController: UIViewController {
     let nameLineStackView = UIStackView(axis: .horizontal)
     let ageLineStackView = UIStackView(axis: .horizontal)
     let speciesLineStackView = UIStackView(axis: .horizontal)
+    
+    let ageList: [String] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+    let agePickerView = UIPickerView()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         nameTextField.delegate = self
-        ageTextField.delegate = self
+        agePickerView.delegate = self
+        agePickerView.dataSource = self
         settingButtons()
-        configureLineStackViews()
         configureStackView()
+        configureLineStackViews()
     }
     
     func settingButtons() {
-        speciesButton.menu = UIMenu(title: "멍냥멍냥??", identifier: nil, options: .singleSelection, children: [self.catAction, self.dogAction])
         startButton.addAction(pushNextAction, for: .touchUpInside)
         startButton.setImage(UIImage(named: "start"), for: .normal)
-        speciesButton.showsMenuAsPrimaryAction = true
         startButton.showsMenuAsPrimaryAction = true
     }
     
-    func settingPet(type: PetType) {
-        guard let name = self.nameTextField.text,
-              let age = self.ageTextField.text else {
-            return
+    func settingMainImage(type: PetType) {
+        self.mainImage.image = UIImage(named: type.randomImage())
+    }
+    
+    func settingPet(type: PetType) -> Pet {
+        guard let name = self.nameTextField.text else {
+            return Pet(name: "secret", age: age, species: type)
         }
         
-        self.pet = Pet(name: name, age: Int(age) ?? 100, species: type)
-        self.logoImage.image = UIImage(named: type.randomImage())
+        return Pet(name: name, age: age, species: type)
     }
     
     func configureLineStackViews() {
@@ -75,18 +83,18 @@ class PetInformationViewController: UIViewController {
         nameLineStackView.addArrangedSubview(nameTextField)
         
         ageLineStackView.addArrangedSubview(ageImage)
-        ageLineStackView.addArrangedSubview(ageTextField)
+        ageLineStackView.addArrangedSubview(agePickerView)
         
         speciesLineStackView.addArrangedSubview(speciesImage)
-        speciesLineStackView.addArrangedSubview(speciesButton)
+        speciesLineStackView.addArrangedSubview(speciesSegmentControl)
         
         NSLayoutConstraint.activate([
-            nameLineStackView.heightAnchor.constraint(equalToConstant: 50),
-            ageLineStackView.heightAnchor.constraint(equalToConstant: 40),
-            speciesLineStackView.heightAnchor.constraint(equalToConstant: 50),
-            nameImage.widthAnchor.constraint(equalToConstant: 100),
-            ageImage.widthAnchor.constraint(equalToConstant: 100),
-            speciesImage.widthAnchor.constraint(equalToConstant: 100)
+            nameLineStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.06),
+            ageLineStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.06),
+            speciesLineStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.06),
+            nameImage.widthAnchor.constraint(equalToConstant: 90),
+            ageImage.widthAnchor.constraint(equalToConstant: 90),
+            speciesImage.widthAnchor.constraint(equalToConstant: 90)
         ])
     }
 
@@ -94,7 +102,7 @@ class PetInformationViewController: UIViewController {
         view.addSubview(stackView)
         stackView.alignment = .fill
         
-        stackView.addArrangedSubview(logoImage)
+        stackView.addArrangedSubview(mainImage)
         stackView.addArrangedSubview(nameLineStackView)
         stackView.addArrangedSubview(ageLineStackView)
         stackView.addArrangedSubview(speciesLineStackView)
@@ -107,9 +115,9 @@ class PetInformationViewController: UIViewController {
         ])
         
         NSLayoutConstraint.activate([
-            logoImage.heightAnchor.constraint(equalToConstant: 270),
-            logoImage.widthAnchor.constraint(equalToConstant: 270),
-            startButton.heightAnchor.constraint(equalToConstant: 80)
+            mainImage.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4),
+            mainImage.widthAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4),
+            startButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2)
         ])
     }
 }
@@ -117,10 +125,26 @@ class PetInformationViewController: UIViewController {
 extension PetInformationViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == nameTextField {
-            ageTextField.becomeFirstResponder()
-        } else {
-            ageTextField.resignFirstResponder()
+            nameTextField.resignFirstResponder()
         }
+        return false
     }
 }
 
+extension PetInformationViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ageList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        age = Int(ageList[row]) ?? 100
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ageList[row]
+    }
+}
