3608109caecc8737c9cd8fe92dd64a8921f04077
mint3382 <mskim0926@naver.com>
2023-10-17 22:36:22 +0900

    feat: ErrorAlertPresentable protocol 추가

---
+++ PetLanguage/PetLanguage/Protocol/ErrorAlertPresentable.swift
@@ -0,0 +1,26 @@
+//
+//  ErrorAlertPresentable.swift
+//  PetLanguage
+//
+//  Created by minsong kim on 10/17/23.
+//
+
+import UIKit
+
+protocol ErrorAlertPresentable where Self: UIViewController { }
+
+extension ErrorAlertPresentable {
+    var closeAction: UIAlertAction {
+        let action = UIAlertAction(title: String(localized: "Close"), style: .cancel)
+        
+        return action
+    }
+    
+    func presentErrorCheckAlert(error: Error) {
+        let alert = UIAlertController(title: "ERROR!", message: error.localizedDescription, preferredStyle: .alert)
+        
+        alert.addAction(closeAction)
+        
+        self.present(alert, animated: true)
+    }
+}
